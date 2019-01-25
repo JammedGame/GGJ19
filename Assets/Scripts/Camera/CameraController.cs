742f8ac6abbb955700344a2b643aada2e24af53f
Misa Jovanovic <misaj@nordeus.com>
2019-01-25 21:33:20 +0100

    Game.Player

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        var player = Game.Player;
+        if (player == null) { return; }
+
+        transform.position = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Game
+{
+    // us Game.Player to get currently active player.
+    private static Player _player;
+    public static Player Player
+    {
+        get
+        {
+            if (_player == null) { _player = GameObject.FindObjectOfType<Player>(); }
+            return _player;
+        }
+    }
+}
