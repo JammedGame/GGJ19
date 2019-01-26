4d5234bfc783c52e3f70d03d59be338a7189ca74
Misa Jovanovic <misaj@nordeus.com>
2019-01-26 14:45:54 +0100

    obstacle initial

---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float RotationSpeedMin;
+    public float RotationSpeedMax;
+
+    [Header("Game State")]
+    public float RotationSpeed;
+}
