488eb756d4c2cade2f03152ace31844be6588f85
VukSDev <vukspalajkovic@gmail.com>
2019-01-26 15:03:51 +0100

    indicator

---
+++ Assets/Scripts/Enemies/Sniper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : Enemy
+{
+    
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        MoveAndOrient();
+    }
+    public override void MoveAndOrient()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+        transform.rotation = Quaternion.RotateTowards
+        (
+            transform.rotation,
+            Quaternion.LookRotation(Vector3.forward, transform.position - target.position),
+            rotSpeed * Time.deltaTime
+        );
+    }
+}
