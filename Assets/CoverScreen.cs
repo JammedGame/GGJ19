91c4b1a5c6596e78b547edc666cb235eea2f79f6
Misa Jovanovic <misaj@nordeus.com>
2019-01-27 16:59:36 +0100

    tutorial cover

--- Assets/CoverScreen.cs
+++ Assets/CoverScreen.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoverScreen : MonoBehaviour
 {
     public float cooldown;
+    public Image CoverImage;
+
     public void Update()
     {
         cooldown += Time.deltaTime;
-        if (cooldown > 3f)
+
+        if (cooldown > 2f)
         {
-            Game.RestartGame();
+            CoverImage.color = new Color(1,1,1, cooldown - 2);
+
+            if (cooldown > 3)
+            {
+                if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Home)
+                    || Input.GetMouseButton(0))
+                    {
+                        Game.RestartGame();
+                    }
+            }
+        }
+        else
+        {
+            CoverImage.color = new Color(1,1,1,0);
         }
     }
 }
