776032a48e1e7b07bace0e4c88bbfeb813ba10de
Misa Jovanovic <misaj@nordeus.com>
2019-01-27 16:04:27 +0100

    cover, balancing

---
+++ Assets/CoverScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverScreen : MonoBehaviour
+{
+    public float cooldown;
+    public void Update()
+    {
+        cooldown += Time.deltaTime;
+        if (cooldown > 3f)
+        {
+            Game.RestartGame();
+        }
+    }
+}
